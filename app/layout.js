6c945bd100586d41994e27eac6097afc833c3a4d
fauzimuh258-ui <fauzimuh258@gmail.com>
2026-06-02 10:25:30 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "ZeyAI",
+  description: "Personal RAG Assistant",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
